2264b6dcc785ba747141e9d545e0d266a0b7688e
CleveTok3125 <97098535+CleveTok3125@users.noreply.github.com>
2023-11-26 14:46:47 +0700

    Update py-secret.py

--- py-secret.py
+++ py-secret.py
@@ -1,6 +1,17 @@
 import base64, random
+from Crypto.Cipher import Blowfish
+from struct import pack
 class secret:
   def encrypt(s, k, r=256):
+    bs = Blowfish.block_size
+    cipher = Blowfish.new(k.encode(), Blowfish.MODE_CBC)
+    plaintext = s.encode()
+    plen = bs - len(plaintext) % bs
+    padding = [plen]*plen
+    padding = pack('b'*plen, *padding)
+    s = cipher.iv + cipher.encrypt(plaintext + padding)
+    s = base64.b64encode(s).decode("utf-8")
+    #
     sections = ""
     for i in range(0, r):
       sections += chr(i)
@@ -30,6 +41,7 @@ class secret:
     ans = ''.join(ans)
     ans = base64.b64encode(bytes(ans, 'utf-8')).decode("utf-8")
     return ans
+
   def decrypt(s, k, r=256):
     s = base64.b64decode(s).decode("utf-8")
 
@@ -59,5 +71,16 @@ class secret:
       if not_in_case:
         ans.append(i)
 
-    ans = ''.join(ans)
-    return ans
+    s = ''.join(ans)
+    s = base64.b64decode(bytes(s, 'utf-8'))
+    bs = Blowfish.block_size
+    ciphertext = s
+    iv = ciphertext[:bs]
+    ciphertext = ciphertext[bs:]
+
+    cipher = Blowfish.new(k.encode(), Blowfish.MODE_CBC, iv)
+    msg = cipher.decrypt(ciphertext)
+
+    last_byte = msg[-1]
+    msg = msg[:- (last_byte if type(last_byte) is int else ord(last_byte))]
+    return msg.decode()
